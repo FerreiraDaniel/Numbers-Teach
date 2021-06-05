6a5eb0d8e845edb27ba87029db46851dd3d3af87
Daniel Ferreira <daniel.dos.santos.ferreira@gmail.com>
2021-06-05 15:18:57 +0200

    Convert build.gradle build.gradle.kts from android_ui_layer

--- Numbers-Teach-Android/android_ui_layer/build.gradle.kts
+++ Numbers-Teach-Android/android_ui_layer/build.gradle.kts
@@ -1,15 +1,31 @@
 plugins {
-    id("com.android.application")
+    id("com.android.library")
     id("kotlin-android")
-    id("com.squareup.sqldelight")
 }
 
 android {
     compileSdk = rootProject.extra["compile_sdk_version"] as Int
     buildToolsVersion = rootProject.extra["build_tools_version"] as String
 
-    // Set both the Java and Kotlin compilers to target Java 8.
+    defaultConfig {
+        minSdk = rootProject.extra["minimum_sdk_version"] as Int
+        targetSdk = rootProject.extra["target_sdk_version"] as Int
 
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        proguardFiles(
+            "proguard-rules.pro"
+        )
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -29,48 +45,13 @@ android {
         resValues = false
         shaders = false
     }
-
-    composeOptions {
-        kotlinCompilerVersion = "1.4.32"
-        kotlinCompilerExtensionVersion = "1.0.0-beta07"
-    }
-
-
-    defaultConfig {
-        applicationId = "com.dferreira.numbers_teach"
-        minSdk = rootProject.extra["minimum_sdk_version"] as Int
-        targetSdk = rootProject.extra["target_sdk_version"] as Int
-        versionCode = 1
-        versionName = "1.0"
-    }
-
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    productFlavors {
-    }
 }
 
 dependencies {
-    implementation("androidx.appcompat:appcompat:${rootProject.extra["appcompat_version"]}")
-    implementation("androidx.preference:preference-ktx:${rootProject.extra["preference_version"]}")
-    implementation("com.google.android.material:material:${rootProject.extra["material_design_version"]}")
-    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.2.0")
-    implementation("com.squareup.sqldelight:android-driver:${rootProject.extra["sqlDelightVersion"]}")
-    implementation(project(":languages_dashboard"))
-    implementation(project(":exercise_icons"))
-    implementation(project(":commons"))
-    implementation(project(":numbers"))
-    implementation(project(":domain_layer"))
+
     implementation("androidx.core:core-ktx:${rootProject.extra["core_version"]}")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:${rootProject.extra["kotlin_version"]}")
+    implementation("androidx.appcompat:appcompat:${rootProject.extra["appcompat_version"]}")
+    implementation("com.google.android.material:material:${rootProject.extra["material_design_version"]}")
     // JetPack compose ui
     implementation("androidx.compose.ui:ui:${rootProject.extra["jetPackComposeVersion"]}")
     // Tooling support (Previews, etc.)
@@ -87,13 +68,9 @@ dependencies {
     implementation("androidx.compose.runtime:runtime-rxjava2:${rootProject.extra["jetPackComposeVersion"]}")
 
     testImplementation("junit:junit:4.13.2")
-    testImplementation("com.squareup.sqldelight:sqlite-driver:${rootProject.extra["sqlDelightVersion"]}")
 
     // UI Tests
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:${rootProject.extra["jetPackComposeVersion"]}")
 
-
-}
-repositories {
-    mavenCentral()
+    testImplementation("junit:junit:4.13.2")
 }
